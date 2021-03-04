7a3f095a845ec2c7740eee33fde1920f952afe33
Ângelo Lovatto <angelolovatto@gmail.com>
2021-03-04 15:00:32 -0300

    chore(experiments): test minimal working experiment
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -1,26 +1,32 @@
 # pylint:disable=missing-docstring
 import pytorch_lightning as pl
 
-from data import build_datamodule
-from models import LightningModel
-from policy import make_worker
+import lqsvg.torch.named as nt
+
+from data import build_datamodule  # pylint:disable=wrong-import-order
+from models import LightningModel  # pylint:disable=wrong-import-order
+from policy import make_worker  # pylint:disable=wrong-import-order
+from utils import suppress_dataloader_warning  # pylint:disable=wrong-import-order
 
 
+@nt.suppress_named_tensor_warning()
 def main():
-    worker = make_worker(num_envs=100)
+    env_config = dict(n_state=2, n_ctrl=2, horizon=100, num_envs=100)
+    worker = make_worker(env_config)
     model = LightningModel(worker.get_policy(), worker.env)
     datamodule = build_datamodule(worker, total_trajs=100)
 
     logger = pl.loggers.WandbLogger(
         name="SVG Prediction",
-        offline=False,
+        offline=True,
         project="LQG-SVG",
         log_model=False,
         entity="angelovtt",
         tags="v0",
     )
-    trainer = pl.Trainer(logger=logger)
-    trainer.fit(model, datamodule=datamodule)
+    trainer = pl.Trainer(logger=logger, max_epochs=20)
+    with suppress_dataloader_warning():
+        trainer.fit(model, datamodule=datamodule)
 
 
 if __name__ == "__main__":
