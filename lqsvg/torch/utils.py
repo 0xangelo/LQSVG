4accd9a2f119467e9076f17b573e70f3183a421f
Ângelo Lovatto <angelolovatto@gmail.com>
2021-03-08 15:26:19 -0300

    feat(torch): add make_spd_matrix
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- lqsvg/torch/utils.py
+++ lqsvg/torch/utils.py
@@ -1,7 +1,13 @@
 # pylint:disable=missing-module-docstring
+from __future__ import annotations
+
+from typing import Optional
+
 import numpy as np
 import torch
 from torch import Tensor
+
+import lqsvg.np_util as np_util
 
 
 def as_float_tensor(array: np.ndarray) -> Tensor:
@@ -11,3 +17,17 @@ def as_float_tensor(array: np.ndarray) -> Tensor:
     not a float32.
     """
     return torch.as_tensor(array, dtype=torch.float32)
+
+
+def make_spd_matrix(
+    n_dim: int,
+    sample_shape: tuple[int, ...],
+    dtype: Optional[torch.dtype] = torch.float32,
+    device: Optional[torch.device] = None,
+) -> Tensor:
+    """PyTorch version of random symmetric positive-definite matrix generation."""
+    return torch.as_tensor(
+        np_util.make_spd_matrix(n_dim, sample_shape=sample_shape),
+        dtype=dtype,
+        device=device,
+    )
