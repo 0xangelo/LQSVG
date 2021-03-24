0538fad2ff27b957765813924d75636a7b7f5473
Ângelo Lovatto <angelolovatto@gmail.com>
2021-03-24 13:12:50 -0300

    chore(envs): explicitly don't sample init covariance for LQG envs
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- lqsvg/envs/lqr/gym.py
+++ lqsvg/envs/lqr/gym.py
@@ -92,7 +92,12 @@ class LQGGenerator(DataClassJsonMixin):
             rng=self._rng,
             Fs_eigval_range=self.Fs_eigval_range,
         )
-        init = make_gaussinit(state_size=self.n_state, n_batch=n_batch, rng=self._rng)
+        init = make_gaussinit(
+            state_size=self.n_state,
+            n_batch=n_batch,
+            sample_covariance=False,
+            rng=self._rng,
+        )
 
         return dynamics, cost, init
 
