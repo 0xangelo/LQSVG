e4fb65877d4fb67bc5f9f9c97026cb98a5657cca
Ângelo Lovatto <angelolovatto@gmail.com>
2021-04-14 10:17:53 -0300

    refactor(envs): unify covariance cholesky modules
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- lqsvg/envs/lqr/modules/dynamics/linear.py
+++ lqsvg/envs/lqr/modules/dynamics/linear.py
@@ -17,26 +17,6 @@ from .common import TVMultivariateNormal, assemble_scale_tril, disassemble_covar
 
 
 class CovarianceCholesky(nn.Module):
-    """Covariance matrix stored as Cholesky factor."""
-
-    beta: float = 0.2
-
-    def __init__(self, sigma: Tensor):
-        super().__init__()
-        assert len(sigma.shape) == 2
-        ltril, pre_diag = nt.unnamed(*disassemble_covariance(sigma, beta=self.beta))
-        self.ltril, self.pre_diag = (nn.Parameter(x) for x in (ltril, pre_diag))
-
-    def _named_factors(self) -> tuple[Tensor, Tensor]:
-        return nt.matrix(self.ltril), nt.vector(self.pre_diag)
-
-    def forward(self) -> Tensor:
-        # pylint:disable=missing-function-docstring
-        ltril, pre_diag = self._named_factors()
-        return nt.matrix(assemble_scale_tril(ltril, pre_diag, beta=self.beta))
-
-
-class CovCholeskyFactor(nn.Module):
     """Covariance Cholesky factor."""
 
     beta: float = 0.2
@@ -61,6 +41,69 @@ class CovCholeskyFactor(nn.Module):
         return assemble_scale_tril(ltril, pre_diag, beta=self.beta)
 
 
+class StationaryCovarianceCholesky(CovarianceCholesky):
+    """Covariance matrix stored as Cholesky factor."""
+
+    def __init__(self, sigma: Tensor):
+        assert len(sigma.shape) == 2
+        sigma = sigma.align_to("H", ...)
+        super().__init__(sigma, horizon=1)
+
+
+class TVLinearNormalParams(nn.Module):
+    """Time-varying linear state-action conditional Gaussian parameters."""
+
+    # pylint:disable=invalid-name
+    # noinspection PyPep8Naming
+    def __init__(self, dynamics: lqr.LinSDynamics, horizon: int):
+        super().__init__()
+
+        F, f, W = dynamics
+        self.F = nn.Parameter(nt.unnamed(F))
+        self.f = nn.Parameter(nt.unnamed(f))
+        self.scale_tril = CovarianceCholesky(W, horizon)
+        self.horizon = horizon
+
+    def _transition_factors(
+        self, index: Optional[IntTensor] = None
+    ) -> Tuple[Tensor, Tensor]:
+        F, f = nt.horizon(nt.matrix(self.F)), nt.horizon(nt.vector(self.f))
+        if index is not None:
+            # Timesteps after termination use last parameters
+            index = torch.clamp(index, max=self.horizon - 1)
+            # noinspection PyTypeChecker
+            F, f = (nt.index_by(x, dim="H", index=index) for x in (F, f))
+        return F, f
+
+    def forward(self, obs: Tensor, action: Tensor):
+        # pylint:disable=missing-function-docstring
+        obs, action = nt.vector(obs), nt.vector(action)
+        state, time = unpack_obs(obs)
+
+        # Get parameters for each timestep
+        index = nt.vector_to_scalar(time)
+        # noinspection PyTypeChecker
+        F, f = self._transition_factors(index)
+        scale_tril = self.scale_tril(index)
+
+        # Compute the loc for normal transitions
+        tau = nt.vector_to_matrix(torch.cat([state, action], dim="R"))
+        trans_loc = nt.matrix_to_vector(F @ tau + nt.vector_to_matrix(f))
+
+        # Treat absorving states if necessary
+        terminal = time.eq(self.horizon)
+        loc = nt.where(terminal, state, trans_loc)
+        time_ = nt.where(terminal, time, time + 1)
+        return {"loc": loc, "scale_tril": scale_tril, "time": time_}
+
+    def as_linsdynamics(self) -> lqr.LinSDynamics:
+        # pylint:disable=missing-function-docstring
+        F, f = self._transition_factors()
+        scale_tril = self.scale_tril()
+        covariance_matrix = scale_tril @ nt.transpose(scale_tril)
+        return lqr.LinSDynamics(F, f, covariance_matrix)
+
+
 # noinspection PyPep8Naming
 class LinearNormalParams(nn.Module):
     """Linear state-action conditional Gaussian parameters."""
@@ -74,7 +117,7 @@ class LinearNormalParams(nn.Module):
         assert isstationary(dynamics)
         self.F = nn.Parameter(nt.unnamed(dynamics.F.select("H", 0)))
         self.f = nn.Parameter(nt.unnamed(dynamics.f.select("H", 0)))
-        self.scale_tril = CovarianceCholesky(dynamics.W.select("H", 0))
+        self.scale_tril = StationaryCovarianceCholesky(dynamics.W.select("H", 0))
         if horizon is None:
             _, _, horizon = lqr.dims_from_dynamics(dynamics)
         self.horizon = horizon
@@ -87,7 +130,7 @@ class LinearNormalParams(nn.Module):
         obs, action = (nt.vector(x) for x in (obs, action))
         state, time = unpack_obs(obs)
         F, f = self._transition_factors()
-        scale_tril = self.scale_tril()
+        scale_tril = self.scale_tril(nt.vector_to_scalar(time))
 
         tau = nt.vector_to_matrix(torch.cat([state, action], dim="R"))
         terminal = time.eq(self.horizon)
@@ -143,60 +186,6 @@ class LinearDynamicsModule(LinearDynamics):
 
     def standard_form(self) -> lqr.LinSDynamics:
         return self.params.as_linsdynamics()
-
-
-class TVLinearNormalParams(nn.Module):
-    """Time-varying linear state-action conditional Gaussian parameters."""
-
-    # pylint:disable=invalid-name
-    # noinspection PyPep8Naming
-    def __init__(self, dynamics: lqr.LinSDynamics, horizon: int):
-        super().__init__()
-
-        F, f, W = dynamics
-        self.F = nn.Parameter(nt.unnamed(F))
-        self.f = nn.Parameter(nt.unnamed(f))
-        self.scale_tril = CovCholeskyFactor(W, horizon)
-        self.horizon = horizon
-
-    def _transition_factors(
-        self, index: Optional[IntTensor] = None
-    ) -> Tuple[Tensor, Tensor]:
-        F, f = nt.horizon(nt.matrix(self.F)), nt.horizon(nt.vector(self.f))
-        if index is not None:
-            # Timesteps after termination use last parameters
-            index = torch.clamp(index, max=self.horizon - 1)
-            # noinspection PyTypeChecker
-            F, f = (nt.index_by(x, dim="H", index=index) for x in (F, f))
-        return F, f
-
-    def forward(self, obs: Tensor, action: Tensor):
-        # pylint:disable=missing-function-docstring
-        obs, action = nt.vector(obs), nt.vector(action)
-        state, time = unpack_obs(obs)
-
-        # Get parameters for each timestep
-        index = nt.vector_to_scalar(time)
-        # noinspection PyTypeChecker
-        F, f = self._transition_factors(index)
-        scale_tril = self.scale_tril(index)
-
-        # Compute the loc for normal transitions
-        tau = nt.vector_to_matrix(torch.cat([state, action], dim="R"))
-        trans_loc = nt.matrix_to_vector(F @ tau + nt.vector_to_matrix(f))
-
-        # Treat absorving states if necessary
-        terminal = time.eq(self.horizon)
-        loc = nt.where(terminal, state, trans_loc)
-        time_ = nt.where(terminal, time, time + 1)
-        return {"loc": loc, "scale_tril": scale_tril, "time": time_}
-
-    def as_linsdynamics(self) -> lqr.LinSDynamics:
-        # pylint:disable=missing-function-docstring
-        F, f = self._transition_factors()
-        scale_tril = self.scale_tril()
-        covariance_matrix = scale_tril @ nt.transpose(scale_tril)
-        return lqr.LinSDynamics(F, f, covariance_matrix)
 
 
 class TVLinearDynamicsModule(LinearDynamics):
