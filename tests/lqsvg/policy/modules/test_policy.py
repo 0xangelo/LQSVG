a3ba0a1874678aaeb6b38d396c42cfac734add99
Ângelo Lovatto <angelolovatto@gmail.com>
2021-04-16 11:51:29 -0300

    test(policy): ensure stabilizing gain for larger state-action sizes
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- tests/lqsvg/policy/modules/test_policy.py
+++ tests/lqsvg/policy/modules/test_policy.py
@@ -14,87 +14,43 @@ from lqsvg.policy.modules import TVLinearFeedback, TVLinearPolicy
 from lqsvg.policy.modules.policy import stabilizing_gain
 
 
-@pytest.fixture
-def dynamics(n_state: int, n_ctrl: int, horizon: int, seed: int) -> lqr.LinSDynamics:
-    dyn = make_lindynamics(
-        n_state,
-        n_ctrl,
-        horizon,
-        stationary=True,
-        passive_eigval_range=(0.5, 1.5),
-        controllable=True,
-        bias=False,
-        rng=seed,
-    )
-    return make_linsdynamics(dyn, stationary=True, rng=seed)
+class TestStabilizingGain:
+    @pytest.fixture(params=(2 ** i for i in range(1, 5)))
+    def n_state(self, request) -> int:
+        return request.param
 
+    @pytest.fixture(params=(2 ** i for i in range(1, 5)))
+    def n_ctrl(self, request) -> int:
+        return request.param
 
-# noinspection PyArgumentList
-def test_stabilizing_gain(dynamics: lqr.LinSDynamics, n_state: int, n_ctrl: int):
-    gain = stabilizing_gain(dynamics)
-    assert torch.is_tensor(gain)
-    assert torch.isfinite(gain).all()
-    assert gain.size("R") == n_ctrl
-    assert gain.size("C") == n_state
-
-    A, B = (x.numpy() for x in stationary_dynamics_factors(dynamics))
-    K = gain.numpy()
-    eigval, _ = np.linalg.eig(A + B @ K)
-    assert np.all(np.abs(eigval) < 1.0)
-
-
-class TestTVLinearPolicy:
     @pytest.fixture
-    def module(self, n_state: int, n_ctrl: int, horizon: int) -> TVLinearPolicy:
-        return TVLinearPolicy(n_state, n_ctrl, horizon)
+    def dynamics(
+        self, n_state: int, n_ctrl: int, horizon: int, seed: int
+    ) -> lqr.LinSDynamics:
+        dyn = make_lindynamics(
+            n_state,
+            n_ctrl,
+            horizon,
+            stationary=True,
+            passive_eigval_range=(0.5, 1.5),
+            controllable=True,
+            bias=False,
+            rng=seed,
+        )
+        return make_linsdynamics(dyn, stationary=True, rng=seed)
 
-    def test_normal_call(self, module: TVLinearPolicy, obs: Tensor, n_ctrl: int):
-        act = module(obs)
+    # noinspection PyArgumentList
+    def test(self, dynamics: lqr.LinSDynamics, n_state: int, n_ctrl: int):
+        gain = stabilizing_gain(dynamics)
+        assert torch.is_tensor(gain)
+        assert torch.isfinite(gain).all()
+        assert gain.size("R") == n_ctrl
+        assert gain.size("C") == n_state
 
-        assert torch.is_tensor(act)
-        assert torch.isfinite(act).all()
-        assert act.names == obs.names
-        assert act.size("R") == n_ctrl
-
-        act.sum().backward()
-        assert obs.grad is not None
-        assert not torch.allclose(obs.grad, torch.zeros_like(obs.grad))
-        assert torch.isfinite(obs.grad).all()
-        grads = [p.grad for p in module.parameters()]
-        assert all(list(g is not None for g in grads))
-        assert all(list(not torch.allclose(g, torch.zeros_like(g)) for g in grads))
-        assert all(list(torch.isfinite(g).all() for g in grads))
-
-    def test_terminal_call(self, module: TVLinearPolicy, last_obs: Tensor, n_ctrl: int):
-        act = module(last_obs)
-
-        assert nt.allclose(act, torch.zeros_like(act))
-        assert torch.is_tensor(act)
-        assert torch.isfinite(act).all()
-        assert act.names == last_obs.names
-        assert act.size("R") == n_ctrl
-
-        act.sum().backward()
-        assert last_obs.grad is not None
-        assert torch.allclose(last_obs.grad, torch.zeros_like(last_obs.grad))
-        grads = [p.grad for p in module.parameters()]
-        assert all(list(g is not None for g in grads))
-        assert all(list(torch.allclose(g, torch.zeros_like(g)) for g in grads))
-
-    def test_mixed_call(self, module: TVLinearPolicy, mix_obs: Tensor, n_ctrl: int):
-        act = module(mix_obs)
-
-        assert torch.is_tensor(act)
-        assert torch.isfinite(act).all()
-        assert act.names == mix_obs.names
-        assert act.size("R") == n_ctrl
-
-        act.sum().backward()
-        assert mix_obs.grad is not None
-        assert torch.isfinite(mix_obs.grad).all()
-        grads = [p.grad for p in module.parameters()]
-        assert all(list(g is not None for g in grads))
-        assert all(list(torch.isfinite(g).all() for g in grads))
+        A, B = (x.numpy() for x in stationary_dynamics_factors(dynamics))
+        K = gain.numpy()
+        eigval, _ = np.linalg.eig(A + B @ K)
+        assert np.all(np.abs(eigval) < 1.0)
 
 
 class TestTVLinearFeedBack:
@@ -151,3 +107,57 @@ class TestTVLinearFeedBack:
         act.sum().backward()
         assert obs.grad is not None
         assert not torch.allclose(obs.grad, torch.zeros_like(obs.grad))
+
+
+class TestTVLinearPolicy:
+    @pytest.fixture
+    def module(self, n_state: int, n_ctrl: int, horizon: int) -> TVLinearPolicy:
+        return TVLinearPolicy(n_state, n_ctrl, horizon)
+
+    def test_normal_call(self, module: TVLinearPolicy, obs: Tensor, n_ctrl: int):
+        act = module(obs)
+
+        assert torch.is_tensor(act)
+        assert torch.isfinite(act).all()
+        assert act.names == obs.names
+        assert act.size("R") == n_ctrl
+
+        act.sum().backward()
+        assert obs.grad is not None
+        assert not torch.allclose(obs.grad, torch.zeros_like(obs.grad))
+        assert torch.isfinite(obs.grad).all()
+        grads = [p.grad for p in module.parameters()]
+        assert all(list(g is not None for g in grads))
+        assert all(list(not torch.allclose(g, torch.zeros_like(g)) for g in grads))
+        assert all(list(torch.isfinite(g).all() for g in grads))
+
+    def test_terminal_call(self, module: TVLinearPolicy, last_obs: Tensor, n_ctrl: int):
+        act = module(last_obs)
+
+        assert nt.allclose(act, torch.zeros_like(act))
+        assert torch.is_tensor(act)
+        assert torch.isfinite(act).all()
+        assert act.names == last_obs.names
+        assert act.size("R") == n_ctrl
+
+        act.sum().backward()
+        assert last_obs.grad is not None
+        assert torch.allclose(last_obs.grad, torch.zeros_like(last_obs.grad))
+        grads = [p.grad for p in module.parameters()]
+        assert all(list(g is not None for g in grads))
+        assert all(list(torch.allclose(g, torch.zeros_like(g)) for g in grads))
+
+    def test_mixed_call(self, module: TVLinearPolicy, mix_obs: Tensor, n_ctrl: int):
+        act = module(mix_obs)
+
+        assert torch.is_tensor(act)
+        assert torch.isfinite(act).all()
+        assert act.names == mix_obs.names
+        assert act.size("R") == n_ctrl
+
+        act.sum().backward()
+        assert mix_obs.grad is not None
+        assert torch.isfinite(mix_obs.grad).all()
+        grads = [p.grad for p in module.parameters()]
+        assert all(list(g is not None for g in grads))
+        assert all(list(torch.isfinite(g).all() for g in grads))
