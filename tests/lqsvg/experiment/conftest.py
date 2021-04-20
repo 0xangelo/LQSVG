57b159d55851ee42a3b522a49728bf91f35cb553
Ângelo Lovatto <angelolovatto@gmail.com>
2021-04-20 10:17:42 -0300

    test(experiment): set torch random state
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- tests/lqsvg/experiment/conftest.py
+++ tests/lqsvg/experiment/conftest.py
@@ -1,6 +1,18 @@
 import pytest
 
 from lqsvg.testing.fixture import standard_fixture
+from lqsvg.torch.utils import default_generator_seed
+
+
+@pytest.fixture
+def seed() -> int:
+    return 123
+
+
+@pytest.fixture(autouse=True)
+def torch_generator_state(seed: int):
+    with default_generator_seed(seed):
+        yield
 
 
 @pytest.fixture
@@ -16,11 +28,6 @@ def n_ctrl() -> int:
 @pytest.fixture
 def horizon() -> int:
     return 20
-
-
-@pytest.fixture
-def seed() -> int:
-    return 123
 
 
 stationary = standard_fixture((True, False), "Stationary")
