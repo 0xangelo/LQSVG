b36484900223c46ab9b74402ca9f238a6261bb1c
Ângelo Lovatto <angelolovatto@gmail.com>
2021-04-22 09:37:37 -0300

    test(policy): check value function from policy
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- tests/lqsvg/policy/modules/test_value.py
+++ tests/lqsvg/policy/modules/test_value.py
@@ -6,7 +6,8 @@ import torch
 from torch import Tensor
 
 import lqsvg.torch.named as nt
-from lqsvg.envs.lqr import Quadratic
+from lqsvg.envs.lqr import Linear, LinSDynamics, QuadCost, Quadratic
+from lqsvg.envs.lqr.generators import make_lindynamics, make_linsdynamics, make_quadcost
 from lqsvg.envs.lqr.utils import random_normal_vector, random_spd_matrix
 from lqsvg.policy.modules.value import QuadQValue, QuadraticMixin, QuadVValue
 
@@ -16,6 +17,26 @@ def check_quadratic_parameters(module: QuadraticMixin, quadratic: Quadratic):
     assert nt.allclose(module.quad, quad)
     assert nt.allclose(module.linear, linear)
     assert nt.allclose(module.const, const)
+
+
+@pytest.fixture
+def policy(n_state: int, n_ctrl: int, horizon: int) -> Linear:
+    K = torch.rand((horizon, n_ctrl, n_state))
+    k = torch.rand((horizon, n_ctrl))
+    K, k = nt.horizon(nt.matrix(K), nt.vector(k))
+    return K, k
+
+
+@pytest.fixture
+def dynamics(n_state: int, n_ctrl: int, horizon: int, seed: int) -> LinSDynamics:
+    lin = make_lindynamics(n_state, n_ctrl, horizon, rng=seed)
+    dyn = make_linsdynamics(lin, rng=seed)
+    return dyn
+
+
+@pytest.fixture
+def cost(n_state: int, n_ctrl: int, horizon: int, seed: int) -> QuadCost:
+    return make_quadcost(n_state, n_ctrl, horizon, rng=seed)
 
 
 # noinspection PyMethodMayBeStatic
@@ -67,6 +88,16 @@ class TestQuadVValue:
         c = random_normal_vector(size=1, horizon=horizon + 1, rng=seed).squeeze("R")
         return V, v, c
 
+    def test_copy_(self, vvalue: QuadVValue, params: Quadratic):
+        old_params = tuple(x.clone() for x in vvalue.standard_form())
+        before = [p.clone() for p in vvalue.parameters()]
+        vvalue.copy_(params)
+        after = [p.clone() for p in vvalue.parameters()]
+
+        allclose_parameters = [torch.allclose(b, a) for b, a in zip(before, after)]
+        allclose_quadratics = [nt.allclose(a, b) for a, b in zip(params, old_params)]
+        assert all(allclose_parameters) == all(allclose_quadratics)
+
     def test_from_existing(self, params: Quadratic, obs: Tensor):
         for par in params:
             par.requires_grad_(True)
@@ -79,15 +110,9 @@ class TestQuadVValue:
         for par in params:
             assert par.grad is None
 
-    def test_copy_(self, vvalue: QuadVValue, params: Quadratic):
-        old_params = tuple(x.clone() for x in vvalue.standard_form())
-        before = [p.clone() for p in vvalue.parameters()]
-        vvalue.copy_(params)
-        after = [p.clone() for p in vvalue.parameters()]
-
-        allclose_parameters = [torch.allclose(b, a) for b, a in zip(before, after)]
-        allclose_quadratics = [nt.allclose(a, b) for a, b in zip(params, old_params)]
-        assert all(allclose_parameters) == all(allclose_quadratics)
+    def test_from_policy(self, policy: Linear, dynamics: LinSDynamics, cost: QuadCost):
+        module = QuadVValue.from_policy(policy, dynamics, cost)
+        assert isinstance(module, QuadVValue)
 
 
 class TestQuadQValue:
@@ -168,3 +193,7 @@ class TestQuadQValue:
         val.sum().backward()
         for par in params:
             assert par.grad is None
+
+    def test_from_policy(self, policy: Linear, dynamics: LinSDynamics, cost: QuadCost):
+        module = QuadQValue.from_policy(policy, dynamics, cost)
+        assert isinstance(module, QuadQValue)
