28deb941bec67b4c34afded273c801004b385a02
angelolovatto <angelolovatto@gmail.com>
2021-07-21 12:02:23 -0300

    feat(experiments): add boilerplate for ch5
    Signed-off-by: angelolovatto <angelolovatto@gmail.com>

--- experiments/ch5/model_training_static_policy.py
+++ experiments/ch5/model_training_static_policy.py
@@ -1,21 +1,139 @@
 # pylint:disable=missing-docstring
+from dataclasses import dataclass
+from typing import Optional, Tuple
+
 import numpy as np
+import pytorch_lightning as pl
 import ray
 import torch
-import wandb
 from ray import tune
-from raylab.policy.modules.model import StochasticModel
-from torch.optim import Optimizer
+from torch import Tensor
+from torch.utils.data import DataLoader, Dataset
 from wandb.sdk import wandb_config
 
 import lqsvg.envs.lqr.utils as lqg_util
 import lqsvg.experiment.utils as utils
 import lqsvg.torch.named as nt
+import wandb
+from lqsvg.envs import lqr
 from lqsvg.envs.lqr.generators import LQGGenerator
 from lqsvg.envs.lqr.modules import LQGModule
-from lqsvg.experiment.estimators import MAAC
+from lqsvg.experiment.data import split_dataset
+from lqsvg.experiment.estimators import MAAC, AnalyticSVG
 from lqsvg.np_util import RNG
 from lqsvg.policy.modules import QuadQValue, TVLinearPolicy
+from lqsvg.policy.modules.transition import (
+    LinearDiagDynamicsModel,
+    SegmentStochasticModel,
+)
+
+BATCH = Tuple[Tensor, Tensor, Tensor]
+
+
+class LightningModel(pl.LightningModule):
+    model: SegmentStochasticModel
+    estimator: MAAC
+    lqg: LQGModule
+    policy: TVLinearPolicy
+    qval: QuadQValue
+    gold_standard: Tuple[Tensor, lqr.Linear]
+
+    def __init__(self, lqg: LQGModule, policy: TVLinearPolicy):
+        super().__init__()
+        self.lqg = lqg
+        self.policy = policy
+        self.qval = QuadQValue(lqg.n_state + lqg.n_ctrl, lqg.horizon)
+        self.qval.match_policy_(
+            policy.standard_form(),
+            lqg.trans.standard_form(),
+            lqg.reward.standard_form(),
+        )
+        self.model = LinearDiagDynamicsModel(
+            lqg.n_state, lqg.n_ctrl, lqg.horizon, stationary=True
+        )
+        self.estimator = MAAC(policy, self.model, lqg.reward, self.qval)
+        self.compute_gold_standards()
+
+    def compute_gold_standards(self):
+        self.gold_standard = AnalyticSVG(self.policy, self.lqg)()
+
+    # noinspection PyArgumentList
+    def forward(self, obs: Tensor, act: Tensor, new_obs: Tensor) -> Tensor:
+        """Log-likelihood of (batched) trajectory segment."""
+        return self.model.seg_log_prob(obs, act, new_obs) / obs.size("H")
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.model.parameters(), lr=self.hparams.learning_rate)
+
+    def _compute_loss_on_batch(self, batch: BATCH, _: int) -> Tensor:
+        obs, act, new_obs = (x.refine_names("B", "H", "R") for x in batch)
+        return -self(obs, act, new_obs).mean()
+
+    def training_step(self, batch: BATCH, batch_idx: int) -> Tensor:
+        # pylint:disable=arguments-differ
+        loss = self._compute_loss_on_batch(batch, batch_idx)
+        self.log("train/loss", loss)
+        return loss
+
+    def validation_step(self, batch: BATCH, batch_idx: int) -> Tensor:
+        # pylint:disable=arguments-differ
+        loss = self._compute_loss_on_batch(batch, batch_idx)
+        grad_acc = self._compute_grad_acc_on_batch(batch)
+        self.log("val/loss", loss)
+        self.log("val/grad_acc", grad_acc)
+        return loss
+
+    def test_step(self, batch: BATCH, batch_idx: int):
+        # pylint:disable=arguments-differ
+        loss = self._compute_loss_on_batch(batch, batch_idx)
+        grad_acc = self._compute_grad_acc_on_batch(batch)
+        self.log("test/loss", loss)
+        self.log("test/grad_acc", grad_acc)
+
+    def _compute_grad_acc_on_batch(self, batch: BATCH) -> Tensor:
+        obs, _, _ = batch
+        obs = obs.flatten(["B", "H"], "B")
+        estim = self.estimator(
+            obs,
+        )
+
+
+@dataclass
+class DataSpec:
+    train_val_split: (float, float)
+    train_batch_size: int
+    val_batch_size: int
+
+
+class DataModule(pl.LightningDataModule):
+    full_dataset: Dataset
+    train_dataset: Dataset
+    val_dataset: Dataset
+
+    def __init__(self, spec: DataSpec):
+        super().__init__()
+        self.spec = spec
+
+    def setup(self, stage: Optional[str] = None):
+        del stage
+        self.train_dataset, self.val_dataset, _ = split_dataset(
+            self.full_dataset, self.spec.train_val_split + (0,)
+        )
+
+    def train_dataloader(self) -> DataLoader:
+        # pylint:disable=arguments-differ
+        spec = self.spec
+        dataloader = DataLoader(
+            self.train_dataset, shuffle=True, batch_size=spec.train_batch_size
+        )
+        return dataloader
+
+    def val_dataloader(self) -> DataLoader:
+        # pylint:disable=arguments-differ
+        dataloader = DataLoader(
+            self.val_dataset, shuffle=False, batch_size=self.spec.val_batch_size
+        )
+        return dataloader
 
 
 # noinspection PyAbstractClass
@@ -26,17 +144,16 @@ class Experiment(tune.Trainable):
     generator: LQGGenerator
     lqg: LQGModule
     policy: TVLinearPolicy
-    qvalue: QuadQValue
-    model: StochasticModel
-    optimizer: Optimizer
-    estimator: MAAC
+    model: LightningModel
+    datamodule: pl.LightningDataModule
+    trainer: pl.Trainer
 
     def setup(self, config: dict):
         self._init_wandb(config)
         self.rng = np.random.default_rng(self.hparams.seed)
         self.make_generator()
         self.make_modules()
-        self.make_optimizer()
+        self.make_trainer()
 
     def _init_wandb(self, config: dict):
         self.run = wandb.init(
@@ -70,13 +187,21 @@ class Experiment(tune.Trainable):
         lqg = LQGModule.from_existing(dynamics, cost, init)
         policy = TVLinearPolicy(lqg.n_state, lqg.n_ctrl, lqg.horizon)
         policy.stabilize_(dynamics, rng=self.rng)
-        qvalue = QuadQValue(lqg.n_state + lqg.n_ctrl, lqg.horizon)
-        self.lqg, self.policy, self.qvalue = lqg, policy, qvalue
-        self.model = None
+        self.lqg, self.policy = lqg, policy
+        self.model = LightningModel(lqg, policy)
 
-    def make_optimizer(self):
-        self.optimizer = torch.optim.Adam(
-            self.model.parameters(), lr=self.hparams.learning_rate
+    def make_trainer(self):
+        logger = pl.loggers.WandbLogger(
+            save_dir=self.run.dir, log_model=False, experiment=self.run
+        )
+        self.trainer = pl.Trainer(
+            default_root_dir=self.run.dir,
+            logger=logger,
+            num_sanity_val_steps=2,
+            max_epochs=self.hparams.max_epochs,
+            progress_bar_refresh_rate=0,  # don't show progress bar for model training
+            weights_summary=None,  # don't print summary before training
+            checkpoint_callback=False,  # don't save last model checkpoint
         )
 
     def step(self) -> dict:
@@ -98,6 +223,9 @@ class Experiment(tune.Trainable):
         }
         self.run.summary.update(tests)
         self.run.summary.update({"passive_eigvals": wandb.Histogram(eigvals)})
+
+    def build_dataset(self):
+        pass
 
     def cleanup(self):
         self.run.finish()
