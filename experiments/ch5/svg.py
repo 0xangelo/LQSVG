f576a6bb237524a5f1fbce92d667e4caf79d4855
angelolovatto <angelolovatto@gmail.com>
2021-09-03 18:01:48 -0300

    chore(ch5): implement basic svg setup
    Signed-off-by: angelolovatto <angelolovatto@gmail.com>

--- experiments/ch5/svg.py
+++ experiments/ch5/svg.py
@@ -2,7 +2,7 @@
 # pylint:disable=missing-docstring
 import logging
 from collections import deque
-from typing import Generator, Sequence
+from typing import Callable, Generator, Sequence
 
 import pytorch_lightning as pl
 import ray
@@ -11,6 +11,8 @@ from ray import tune
 from wandb_util import WANDB_DIR, wandb_init
 
 from lqsvg import data
+from lqsvg.envs import lqr
+from lqsvg.torch import named as nt
 from lqsvg.torch.nn import LQGModule, TVLinearPolicy
 
 
@@ -20,18 +22,15 @@ def make_model(
     pass
 
 
-def optimize_model_(
-    model: pl.LightningModule, dataset: Sequence[data.Trajectory], config: dict
-) -> dict:
+def model_trainer(
+    config: dict,
+) -> Callable[[pl.LightningModule, Sequence[data.Trajectory]], dict]:
     pass
 
 
-def optimize_policy_(
-    policy: TVLinearPolicy,
-    model: pl.LightningModule,
-    dataset: Sequence[data.Trajectory],
+def policy_trainer(
     config: dict,
-) -> dict:
+) -> Callable[[TVLinearPolicy, pl.LightningModule, Sequence[data.Trajectory]], dict]:
     pass
 
 
@@ -39,18 +38,22 @@ def episode_stats(dataset: Sequence[data.Trajectory]) -> dict:
     pass
 
 
-def policy_optimization_(lqg: LQGModule, policy: TVLinearPolicy, config: dict):
+def policy_optimization_(
+    lqg: LQGModule, policy: TVLinearPolicy, config: dict
+) -> Generator[dict, None, None]:
     """Trains a policy via Stochastic Value Gradients."""
     collect = data.environment_sampler(lqg)
-    model = make_model(lqg, policy, config)
+    optimize_model_ = model_trainer(config)
+    optimize_policy_ = policy_trainer(config)
 
+    model = make_model(lqg, policy, config)
     dataset = deque(maxlen=config["replay_size"] // lqg.horizon)
     for i in range(config["iterations"]):
         metrics = {}
         trajectories = collect(policy, config["trajs_per_iter"])
         dataset.append(trajectories)
-        metrics.update(optimize_model_(model, dataset, config))
-        metrics.update(optimize_policy_(policy, model, dataset, config))
+        metrics.update(optimize_model_(model, dataset))
+        metrics.update(optimize_policy_(policy, model, dataset))
 
         metrics.update(episode_stats(dataset))
         metrics.update(done=(i + 1 == config["iterations"]))
@@ -58,11 +61,17 @@ def policy_optimization_(lqg: LQGModule, policy: TVLinearPolicy, config: dict):
 
 
 def create_env(config: dict) -> LQGModule:
-    pass
+    generator = lqr.LQGGenerator(
+        stationary=True, controllable=True, rng=config["seed"], **config["env_config"]
+    )
+    dynamics, cost, init = generator()
+    return LQGModule.from_existing(dynamics, cost, init).requires_grad_(False)
 
 
 def init_policy(lqg: LQGModule, config: dict) -> TVLinearPolicy:
-    pass
+    return TVLinearPolicy(lqg.n_state, lqg.n_ctrl, lqg.horizon).stabilize_(
+        lqg.trans.standard_form(), rng=config["seed"]
+    )
 
 
 class Experiment(tune.Trainable):
@@ -71,7 +80,8 @@ class Experiment(tune.Trainable):
 
     def setup(self, config: dict):
         pl.seed_everything(config["seed"])
-        lqg = create_env(config)
+        with nt.suppress_named_tensor_warning():
+            lqg = create_env(config)
         policy = init_policy(lqg, config)
         self.coroutine = policy_optimization_(lqg, policy, config)
 
