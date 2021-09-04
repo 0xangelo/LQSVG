440a5e4220a2a167112e32bccd8391144b4a7190
angelolovatto <angelolovatto@gmail.com>
2021-09-04 06:03:11 -0300

    chore(ch5): implement episode_stats
    Signed-off-by: angelolovatto <angelolovatto@gmail.com>

--- experiments/ch5/svg.py
+++ experiments/ch5/svg.py
@@ -2,13 +2,15 @@
 # pylint:disable=missing-docstring
 import logging
 from collections import deque
+from functools import partial
 from typing import Callable, Generator, Sequence
 
 import pytorch_lightning as pl
 import ray
+import torch
 import wandb.sdk
 from ray import tune
-from wandb_util import WANDB_DIR, wandb_init
+from wandb_util import WANDB_DIR, wandb_init, with_prefix
 
 from lqsvg import data
 from lqsvg.envs import lqr
@@ -19,23 +21,50 @@ from lqsvg.torch.nn import LQGModule, TVLinearPolicy
 def make_model(
     lqg: LQGModule, policy: TVLinearPolicy, config: dict
 ) -> pl.LightningModule:
-    pass
+    # pylint:disable=unused-argument
+    return pl.LightningModule()
 
 
 def model_trainer(
     config: dict,
 ) -> Callable[[pl.LightningModule, Sequence[data.Trajectory]], dict]:
-    pass
+    # pylint:disable=unused-argument
+    def train(model: pl.LightningModule, dataset: Sequence[data.Trajectory]) -> dict:
+        pass
+
+    return train
 
 
 def policy_trainer(
     config: dict,
 ) -> Callable[[TVLinearPolicy, pl.LightningModule, Sequence[data.Trajectory]], dict]:
-    pass
+    # pylint:disable=unused-argument
+    def optimize(
+        policy: TVLinearPolicy,
+        model: pl.LightningModule,
+        dataset: Sequence[data.Trajectory],
+    ) -> dict:
+        pass
+
+    return optimize
 
 
 def episode_stats(dataset: Sequence[data.Trajectory]) -> dict:
-    pass
+    batched = map(partial(torch.cat, dim="B"), zip(*dataset))
+    obs, act, rew, logp = (t.align_to("B", "H", ...)[-100:] for t in batched)
+
+    rets = rew.sum("H")
+    log_likelihoods = logp.sum("H")
+    return {
+        "obs_mean": obs.mean().item(),
+        "obs_std": obs.std().item(),
+        "act_mean": act.mean().item(),
+        "act_std": act.std().item(),
+        "episode_return_max": rets.max().item(),
+        "episode_return_mean": rets.mean().item(),
+        "episode_return_min": rets.min().item(),
+        "episode_logp_mean": log_likelihoods.mean().item(),
+    }
 
 
 def policy_optimization_(
@@ -55,7 +84,7 @@ def policy_optimization_(
         metrics.update(optimize_model_(model, dataset))
         metrics.update(optimize_policy_(policy, model, dataset))
 
-        metrics.update(episode_stats(dataset))
+        metrics.update(with_prefix("collect/", episode_stats(dataset)))
         metrics.update(done=(i + 1 == config["iterations"]))
         yield metrics
 
