e8a1db738c976f537d4f4d0fed1fb74137b5f067
angelolovatto <angelolovatto@gmail.com>
2021-09-06 20:39:29 -0300

    chore(ch5): add more boilerplate to svg
    Signed-off-by: angelolovatto <angelolovatto@gmail.com>

--- experiments/ch5/svg.py
+++ experiments/ch5/svg.py
@@ -3,44 +3,103 @@
 import logging
 from collections import deque
 from functools import partial
-from typing import Callable, Generator, Sequence
+from typing import Callable, Generator, Sequence, Tuple
 
+import numpy as np
 import pytorch_lightning as pl
 import ray
 import torch
 import wandb.sdk
+from critic import modules as critic_modules
+from model import make_model as dynamics_model
 from ray import tune
-from torch import nn
+from torch import Tensor, nn
 from wandb_util import WANDB_DIR, wandb_init, with_prefix
 
-from lqsvg import data
+from lqsvg import data, lightning
 from lqsvg.envs import lqr
 from lqsvg.torch import named as nt
-from lqsvg.torch.nn import LQGModule, TVLinearPolicy
+from lqsvg.torch.nn import LQGModule, QuadQValue, QuadRewardModel, TVLinearPolicy
+from lqsvg.torch.sequence import log_prob_fn
+
+DynamicsBatch = Tuple[Tensor, Tensor, Tensor]
+RewardBatch = Tuple[Tensor, Tensor, Tensor]
 
 
-def make_model(lqg: LQGModule, policy: TVLinearPolicy, config: dict) -> nn.Module:
+def make_model(
+    lqg: LQGModule, policy: TVLinearPolicy, rng: Generator, config: dict
+) -> nn.Module:
     # pylint:disable=unused-argument
-    return nn.Module()
+    model = nn.Module()
+    if config["perfect_model"]:
+        model.dynamics = lqg.trans
+        model.reward = lqg.reward
+        model.qval = QuadQValue(
+            n_tau=lqg.n_state + lqg.n_ctrl, horizon=lqg.horizon, rng=rng
+        )
+    else:
+        model.dynamics = dynamics_model(
+            lqg.n_state, lqg.n_ctrl, lqg.horizon, config["dynamics"]
+        )
+        model.reward = QuadRewardModel(
+            lqg.n_state, lqg.n_ctrl, lqg.horizon, stationary=True, rng=rng
+        )
+        model.qval, model.target_qval, model.target_vval = critic_modules(
+            policy, config["qvalue"], rng
+        )
+    return model
+
+
+def dynamics_loss(dynamics: nn.Module) -> Callable[[DynamicsBatch], Tensor]:
+    log_prob = log_prob_fn(dynamics, dynamics.dist)
+
+    def loss(batch: DynamicsBatch) -> Tensor:
+        obs, act, new_obs = (t.refine_names("B", "H", "R") for t in batch)
+        return -log_prob(obs, act, new_obs).mean()
+
+    return loss
+
+
+def reward_loss(reward: nn.Module) -> Callable[[RewardBatch], Tensor]:
+    def loss(batch: RewardBatch) -> Tensor:
+        obs, act, rew = batch
+        obs = obs.refine_names("B", "R")
+        act = act.refine_names("B", "R")
+        rew = rew.refine_names("B")
+        return torch.mean(0.5 * torch.square(reward(obs, act) - rew))
+
+    return loss
 
 
 def model_trainer(
-    config: dict,
-) -> Callable[[nn.Module, Sequence[data.Trajectory]], dict]:
-    # pylint:disable=unused-argument
-    def train(model: nn.Module, dataset: Sequence[data.Trajectory]) -> dict:
-        pass
+    model: nn.Module, config: dict
+) -> Callable[[Sequence[data.Trajectory]], dict]:
+    if config["perfect_model"]:
+        return lambda x: {}
+
+    pl_dynamics = lightning.Lightning(
+        model.dynamics, dynamics_loss(model.dynamics), config
+    )
+    pl_reward = lightning.Lightning(model.reward, reward_loss(model.reward), config)
+
+    def train(dataset: Sequence[data.Trajectory]) -> dict:
+        dynamics_dm = ...
+        reward_dm = ...
+        dynamics_info = lightning.train_lite(pl_dynamics, dynamics_dm, config)
+        reward_info = lightning.train_lite(pl_reward, reward_dm, config)
+        return {
+            **with_prefix("dynamics/", dynamics_info),
+            **with_prefix("reward/", reward_info),
+        }
 
     return train
 
 
 def policy_trainer(
-    config: dict,
-) -> Callable[[TVLinearPolicy, nn.Module, Sequence[data.Trajectory]], dict]:
+    policy: TVLinearPolicy, model: nn.Module, config: dict
+) -> Callable[[Sequence[data.Trajectory]], dict]:
     # pylint:disable=unused-argument
-    def optimize(
-        policy: TVLinearPolicy, model: nn.Module, dataset: Sequence[data.Trajectory]
-    ) -> dict:
+    def optimize(dataset: Sequence[data.Trajectory]) -> dict:
         pass
 
     return optimize
@@ -68,18 +127,21 @@ def policy_optimization_(
     lqg: LQGModule, policy: TVLinearPolicy, config: dict
 ) -> Generator[dict, None, None]:
     """Trains a policy via Stochastic Value Gradients."""
-    collect = data.environment_sampler(lqg)
-    optimize_model_ = model_trainer(config)
-    optimize_policy_ = policy_trainer(config)
+    model = make_model(
+        lqg, policy, np.random.default_rng(config["seed"]), config["model"]
+    )
 
-    model = make_model(lqg, policy, config)
+    collect = data.environment_sampler(lqg)
+    optimize_model_ = model_trainer(model, config)
+    optimize_policy_ = policy_trainer(policy, model, config)
+
     dataset = deque(maxlen=config["replay_size"] // lqg.horizon)
     for i in range(config["iterations"]):
         metrics = {}
         trajectories = collect(policy, config["trajs_per_iter"])
         dataset.append(trajectories)
-        metrics.update(optimize_model_(model, dataset))
-        metrics.update(optimize_policy_(policy, model, dataset))
+        metrics.update(optimize_model_(dataset))
+        metrics.update(optimize_policy_(dataset))
 
         metrics.update(with_prefix("collect/", episode_stats(dataset)))
         metrics.update(done=(i + 1 == config["iterations"]))
